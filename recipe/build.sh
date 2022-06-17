09a1e49665f60c84fae6e77373dca5767be90cab
Nicholas Devenish <ndevenish@gmail.com>
2022-06-17 21:44:20 +0100

    Dont require pager at build time

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,21 +2,9 @@
 
 set -x -e
 
-export INIT=''
+./configure --prefix=$PREFIX  --with-tcl=$PREFIX/lib
 
-if [ "$(uname)" == "Darwin" ]; then
-	export INSTALL_ROOT=${PREFIX}
-	./configure --prefix=$PREFIX  --with-tcl=$PREFIX/lib
-	make prefix=$PREFIX
-	make prefix=$PREFIX install
-  INIT=${PREFIX}/init/bash
-
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	./configure --prefix=$PREFIX --with-tcl=$PREFIX/lib --with-tclsh=$PREFIX/bin/tclsh
-	make
-	make install
-	INIT=${PREFIX}/init/bash
-fi
+INIT=${PREFIX}/init/bash
 
 mkdir -p $PREFIX/etc/conda/activate.d/
 echo "source $INIT" >> $PREFIX/etc/conda/activate.d/environment-modules-activate.sh
